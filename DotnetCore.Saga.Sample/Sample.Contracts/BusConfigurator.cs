cee6340a3ea44348e208a65204961ff3216c500f
Arnold Gulyas <agulyas.ext@graphisoft.com>
2022-12-12 03:23:27 +0100

    Upgrade to .net 6

--- DotnetCore.Saga.Sample/Sample.Contracts/BusConfigurator.cs
+++ DotnetCore.Saga.Sample/Sample.Contracts/BusConfigurator.cs
@@ -6,19 +6,19 @@ namespace Sample.Contracts
 {
     public static class BusConfigurator
     {
-        public static IBusControl ConfigureBus(Action<IRabbitMqBusFactoryConfigurator, IRabbitMqHost> registrationAction = null)
+        public static IBusControl ConfigureBus(Action<IRabbitMqBusFactoryConfigurator> registrationAction = null)
         {
             return Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
                 cfg.Durable = true;
                 cfg.PrefetchCount = 1;
                 cfg.PurgeOnStartup = true;
-                var host = cfg.Host(new Uri(RabbitMqConstants.RabbitMqUri), hst =>
+                cfg.Host(new Uri(RabbitMqConstants.RabbitMqUri), hst =>
                 {
                     hst.Username(RabbitMqConstants.UserName);
                     hst.Password(RabbitMqConstants.Password);
                 });
-                registrationAction?.Invoke(cfg, host);
+                registrationAction?.Invoke(cfg);
             });
         }
     }
--- DotnetCore.Saga.Sample/Sample.ReportTracking/ReportSagaState.cs
+++ DotnetCore.Saga.Sample/Sample.ReportTracking/ReportSagaState.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Text;
-using Automatonymous;
+using MassTransit;
 
 namespace Sample.ReportTracking
 {
